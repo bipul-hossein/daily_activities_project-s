ef5e997da215aa56f4495209390a8021e20e35de
Bipul-hossen <mdbipulhossain252@gmail.com>
2022-09-29 15:40:01 +0600

    customize disign

--- src/Components/Activity/Activity.js
+++ src/Components/Activity/Activity.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './Activity.css'
 
 const Activity = (props) => {
-   const { handleAddToCart}=props
-    const {img, name, activities_details, age, time_required} = props.singleActivity
+    const { handleAddToCart } = props
+    const { img, name, activities_details, age, time_required } = props.singleActivity
     return (
+     
         <div className='activity'>
-           <img src={img} alt="" />
-           <p>{name}</p>
-           <p>{activities_details.slice(0, 79)}</p>
-           <p>For Age : {age}</p>
-           <p>Time required : {time_required}</p>
-           <button className='btn' onClick={()=>handleAddToCart(time_required)}>Add To List</button>
-        
+            <img src={img} alt="" />
+            <p>{name}</p>
+            <p>{activities_details.slice(0, 79)}</p>
+            <p>For Age : {age}</p>
+            <p>Time required : {time_required}</p>
+            <button className='btn' onClick={() => handleAddToCart(time_required)}>Add To List</button>
+
         </div>
+
     );
 };
 
